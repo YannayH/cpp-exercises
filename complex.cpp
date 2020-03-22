d7514e17c8871065b93fa84642152fbcfe844992
Yannay <yannayha@gmail.com>
2020-03-22 19:19:01 +0200

    Changed some methods to be constants, cleaned up the class's and tests code

--- complex.cpp
+++ complex.cpp
@@ -1,47 +1,41 @@
 #include "complex.h"
 
 
-template < class T >Complex< T >::Complex() : real(0), complex(0) {};
-template < class T > Complex< T >::Complex(T real, T complex) : real(real), complex(complex) {};
-template < class T > Complex< T >::Complex(const Complex< T >& complex) : real(complex.real), complex(complex.complex) {};
+template < class T > Complex< T >::Complex() : real(0), complex(0) {}
+template < class T > Complex< T >::Complex(T real, T complex) : real(real), complex(complex) {}
+template < class T > Complex< T >::Complex(const Complex< T >& complex) : real(complex.real), complex(complex.complex) {}
 
 template < class T > Complex< T >& Complex< T >::operator=(const Complex& other_complex) {
     this->real = other_complex.real;
     this->complex = other_complex.complex;
-
     return *this;
 }
 
 template < class T > Complex< T >& Complex< T >::operator+=(const Complex& other_complex) {
     this->real += other_complex.real;
     this->complex += other_complex.complex;
-
     return *this;
 }
 
 template < class T > Complex< T >& Complex< T >::operator+=(T added_real) {
     this->real += added_real;
-
     return *this;
 }
 
 template < class T > Complex< T >& Complex< T >::operator-=(const Complex& other_complex) {
     this->real -= other_complex.real;
     this->complex -= other_complex.complex;
-
     return *this;
 }
 
 template < class T > Complex< T >& Complex< T >::operator-=(T subtracted_real) {
     this->real -= subtracted_real;
-
     return *this;
 }
 
 template < class T > Complex< T >& Complex< T >::operator*=(T scalar) {
     this->real *= scalar;
     this->complex *= scalar;
-
     return *this;
 }
 
--- complex_tests.cpp
+++ complex_tests.cpp
@@ -6,164 +6,146 @@
 
 
 TEST(ConstructorTests, DefaultConstructor) {
-    Complex<double long> complex;
+    Complex<double> complex;
 
-    EXPECT_EQ(complex.real, 0) << "Complex's real value is not 0 in default constructor!";
-    EXPECT_EQ(complex.complex, 0) << "Complex's complex value is not 0 in default constructor!";
+    EXPECT_EQ(complex.real, 0);
+    EXPECT_EQ(complex.complex, 0);
 }
 
 TEST(ConstructorTests, FullConstructor) {
-    double long real_value = 3;
-    double long complex_value = 4;
-    Complex<double long> complex(real_value, complex_value);
+    double real_value = 3;
+    double complex_value = 4;
+    Complex<double> complex(real_value, complex_value);
 
-    EXPECT_EQ(complex.real, real_value) << "Complex's real value is not expected in full constructor!";
-    EXPECT_EQ(complex.complex, complex_value) << "Complex's default value is not expected in full constructor!";
+    EXPECT_EQ(complex.real, real_value);
+    EXPECT_EQ(complex.complex, complex_value);
 }
 
 TEST(ConstructorTests, CopyConstructor) {
-    double long real_value = 3;
-    double long complex_value = 4;
-    Complex<double long> complex(real_value, complex_value);
-    Complex<double long> copied_complex(complex);
+    double real_value = 3;
+    double complex_value = 4;
+    Complex<double> complex(real_value, complex_value);
+    Complex<double> copied_complex(complex);
 
-    EXPECT_EQ(copied_complex.real, real_value) << "Complex's real value is not expected in copy constructor!";
-    EXPECT_EQ(copied_complex.complex, complex_value) << "Complex's default value is not expected in copy constructor!";
+    EXPECT_EQ(copied_complex.real, real_value);
+    EXPECT_EQ(copied_complex.complex, complex_value);
 }
 
 TEST(OperatorTests, Assignment) {
-    double long real_value = 3;
-    double long complex_value = 4;
-    Complex<double long> complex(real_value, complex_value);
-    Complex<double long> copied_complex;
-
+    double real_value = 3;
+    double complex_value = 4;
+    Complex<double> complex(real_value, complex_value);
+    Complex<double> copied_complex;
     copied_complex = complex;
 
-    EXPECT_EQ(copied_complex.real, real_value) << "Complex's real value is not expected in assignment!";
-    EXPECT_EQ(copied_complex.complex, complex_value) << "Complex's default value is not expected in assignment!";
+    EXPECT_EQ(copied_complex.real, real_value);
+    EXPECT_EQ(copied_complex.complex, complex_value);
 }
 
 TEST(OperatorTests, AddComplexes) {
-    Complex<double long> complex1(1, 2);
-    Complex<double long> complex2(5, 5);
+    Complex<double> complex1(1, 2);
+    Complex<double> complex2(5, 5);
+    Complex<double> combined_complex = complex1 + complex2;
 
-    Complex<double long> combined_complex = complex1 + complex2;
-
-    EXPECT_EQ(combined_complex.real, complex1.real + complex2.real) << "Complex's real value is unexpected in Complexes addition!";
-    EXPECT_EQ(combined_complex.complex, complex1.complex + complex2.complex) << "Complex's complex value is unexpected in Complexes addition!";
+    EXPECT_EQ(combined_complex.real, complex1.real + complex2.real);
+    EXPECT_EQ(combined_complex.complex, complex1.complex + complex2.complex);
 }
 
 TEST(OperatorTests, AddReal) {
-    double long real_addition = 3;
-    Complex<double long> complex(1, 2);
+    double real_addition = 3;
+    Complex<double> complex(1, 2);
+    Complex<double> combined_complex = complex + real_addition;
 
-    Complex<double long> combined_complex = complex + real_addition;
-
-    EXPECT_EQ(combined_complex.real, complex.real + real_addition) << "Complex's real value is unexpected in number addition!";
-    EXPECT_EQ(combined_complex.complex, complex.complex) << "Complex's complex value is unexpected in number addition!";
+    EXPECT_EQ(combined_complex.real, complex.real + real_addition);
+    EXPECT_EQ(combined_complex.complex, complex.complex);
 }
 
 TEST(OperatorTests, iAddComplexes) {
-    double long initial_real = 5;
-    double long inital_complex = 5;
-
-    Complex<double long> complex1(1, 2);
-    Complex<double long> complex2(initial_real, inital_complex);
-
+    double initial_real = 5;
+    double inital_complex = 5;
+    Complex<double> complex1(1, 2);
+    Complex<double> complex2(initial_real, inital_complex);
     complex2 += complex1;
 
-    EXPECT_EQ(complex2.real, complex1.real + initial_real) << "Complex's real value is unexpected in in-place Complexes addition!";
-    EXPECT_EQ(complex2.complex, complex1.complex + inital_complex) << "Complex's complex value is unexpected in in-place Complexes addition!";
+    EXPECT_EQ(complex2.real, complex1.real + initial_real);
+    EXPECT_EQ(complex2.complex, complex1.complex + inital_complex);
 }
 
 TEST(OperatorTests, iAddReal) {
-    double long initial_real = 5;
-    double long inital_complex = 5;
-    double long real_addition = 3;
-
-    Complex<double long> complex(initial_real, inital_complex);
+    double initial_real = 5;
+    double inital_complex = 5;
+    double real_addition = 3;
+    Complex<double> complex(initial_real, inital_complex);
     complex += real_addition;
 
-    EXPECT_EQ(complex.real, initial_real + real_addition) << "Complex's real value is unexpected in in-place number addition!";
-    EXPECT_EQ(complex.complex, inital_complex) << "Complex's complex value is unexpected in in-place number addition!";
+    EXPECT_EQ(complex.real, initial_real + real_addition);
+    EXPECT_EQ(complex.complex, inital_complex);
 }
 
 TEST(OperatorTests, SubtractComplexes) {
-    Complex<double long> complex1(1, 2);
-    Complex<double long> complex2(5, 5);
+    Complex<double> complex1(1, 2);
+    Complex<double> complex2(5, 5);
+    Complex<double> subtracted_complex = complex1 - complex2;
 
-    Complex<double long> subtracted_complex = complex1 - complex2;
-
-    EXPECT_EQ(subtracted_complex.real, complex1.real - complex2.real) << "Complex's real value is unexpected in Complexes subtraction!";
-    EXPECT_EQ(subtracted_complex.complex, complex1.complex - complex2.complex) << "Complex's complex value is unexpected in Complexes subtraction!";
+    EXPECT_EQ(subtracted_complex.real, complex1.real - complex2.real);
+    EXPECT_EQ(subtracted_complex.complex, complex1.complex - complex2.complex);
 }
 
 TEST(OperatorTests, SubtractReal) {
-    double long real_addition = 3;
-    Complex<double long> complex(1, 2);
+    double real_addition = 3;
+    Complex<double> complex(1, 2);
+    Complex<double> subtracted_complex = complex - real_addition;
 
-    Complex<double long> subtracted_complex = complex - real_addition;
-
-    EXPECT_EQ(subtracted_complex.real, complex.real - real_addition) << "Complex's real value is unexpected in number subtraction!";
-    EXPECT_EQ(subtracted_complex.complex, complex.complex) << "Complex's complex value is unexpected in number subtraction!";
+    EXPECT_EQ(subtracted_complex.real, complex.real - real_addition);
+    EXPECT_EQ(subtracted_complex.complex, complex.complex);
 }
 
 TEST(OperatorTests, iSubtractComplexes) {
-    double long initial_real = 5;
-    double long inital_complex = 5;
-
-    Complex<double long> complex1(1, 2);
-    Complex<double long> complex2(initial_real, inital_complex);
-
+    double initial_real = 5;
+    double inital_complex = 5;
+    Complex<double> complex1(1, 2);
+    Complex<double> complex2(initial_real, inital_complex);
     complex2 -= complex1;
 
-    EXPECT_EQ(complex2.real, initial_real - complex1.real) << "Complex's real value is unexpected in in-place Complexes addition!";
-    EXPECT_EQ(complex2.complex, inital_complex - complex1.complex) << "Complex's complex value is unexpected in in-place Complexes addition!";
+    EXPECT_EQ(complex2.real, initial_real - complex1.real);
+    EXPECT_EQ(complex2.complex, inital_complex - complex1.complex);
 }
 
 TEST(OperatorTests, iSubtractReal) {
-    double long initial_real = 5;
-    double long inital_complex = 5;
-    double long real_addition = 3;
-
-    Complex<double long> complex(initial_real, inital_complex);
+    double initial_real = 5;
+    double inital_complex = 5;
+    double real_addition = 3;
+    Complex<double> complex(initial_real, inital_complex);
     complex -= real_addition;
 
-    EXPECT_EQ(complex.real, initial_real - real_addition) << "Complex's real value is unexpected in in-place number subtraction!";
-    EXPECT_EQ(complex.complex, inital_complex) << "Complex's complex value is unexpected in in-place number subtraction!";
+    EXPECT_EQ(complex.real, initial_real - real_addition);
+    EXPECT_EQ(complex.complex, inital_complex);
 }
 
 TEST(OperatorTests, MultiplyScalar) {
-    double long scalar = 3;
-    Complex<double long> complex(1, 2);
+    double scalar = 3;
+    Complex<double> complex(1, 2);
+    Complex<double> multiplied_complex = complex * scalar;
 
-    Complex<double long> multiplied_complex = complex * scalar;
-
-    EXPECT_EQ(multiplied_complex.real, complex.real * scalar) << "Complex's real value is unexpected in scalar multiplication!";
-    EXPECT_EQ(multiplied_complex.complex, complex.complex * scalar) << "Complex's complex value is unexpected in scalar multiplication!";
+    EXPECT_EQ(multiplied_complex.real, complex.real * scalar);
+    EXPECT_EQ(multiplied_complex.complex, complex.complex * scalar);
 }
 
 TEST(OperatorTests, iMultiplyScalar) {
-    double long initial_real = 5;
-    double long inital_complex = 5;
-    double long scalar = 3;
-
-    Complex<double long> complex(initial_real, inital_complex);
+    double initial_real = 5;
+    double inital_complex = 5;
+    double scalar = 3;
+    Complex<double> complex(initial_real, inital_complex);
     complex *= scalar;
 
-    EXPECT_EQ(complex.real, initial_real * scalar) << "Complex's real value is unexpected in in-place scalar multiplication!";
-    EXPECT_EQ(complex.complex, inital_complex * scalar) << "Complex's complex value is unexpected in in-place scalar multiplication!";
+    EXPECT_EQ(complex.real, initial_real * scalar);
+    EXPECT_EQ(complex.complex, inital_complex * scalar);
 }
 
 TEST(OperatorTests, OutputToOStream) {
-    testing::internal::CaptureStdout();
-    Complex<double long> complex(4,3);
-    std::cout << complex;
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, std::to_string(complex.real) + " + " + std::to_string(complex.complex) + "i");
-}
+    Complex<double> complex(4,3);
+    std::stringstream output;
+    output << complex;
 
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    EXPECT_EQ(output.str(), std::to_string(complex.real) + " + " + std::to_string(complex.complex) + "i");
 }
